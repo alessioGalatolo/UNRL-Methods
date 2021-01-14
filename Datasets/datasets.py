Fixed error in link probability calculation

Datasets/datasets.py
@@ -1,5 +1,5 @@
 from enum import Enum, auto
-from networkx import Graph, is_weighted
+from networkx import Graph, DiGraph, is_weighted
 
 #list of available datasets
 class Datasets(Enum):
@@ -17,7 +17,7 @@ class Datasets(Enum):
 
 class Formats(Enum):
     mat = auto()
-    txt = auto()
+    txt = auto() #all graphs with this format are directed
     other = auto()
     
 #given the dataset name will return the filename
@@ -48,11 +48,24 @@ dataset2format = {Datasets.Twitter: Formats.txt,
                     Datasets.Pubmed: "Datasets/Pubmed-Diabetes/" #todo
                 }
 
+dataset2directionality = {Datasets.Twitter: DiGraph(),
+                            Datasets.BlogCatalog: Graph(),
+                            Datasets.YouTube: Graph(),
+                            Datasets.Flickr: Graph(),
+                            Datasets.CitHepPh: DiGraph(),
+                            Datasets.Reddit: Graph(),
+                            Datasets.Cora: DiGraph(),
+                            Datasets.Epinions: DiGraph(),
+                            Datasets.Google: DiGraph(),
+                            Datasets.WikiVote: DiGraph(),
+                            Datasets.Pubmed: DiGraph()
+                        }
+
 def get_graph(dataset: Datasets):
     #datasets in mat format
     filename = dataset2filename[dataset]
     format = dataset2format[dataset]
-    graph = Graph() #the dataset to return 
+    graph = dataset2directionality[dataset] #the graph to return 
     
     if format is Formats.mat:
         from scipy.io import loadmat
@@ -65,11 +78,11 @@ def get_graph(dataset: Datasets):
         #TODO use group as labels
     elif format is Formats.txt:
         from networkx import read_edgelist
-        graph = read_edgelist(filename, create_using=Graph(), nodetype=int, data=(('weight',float),))
+        graph = read_edgelist(filename, create_using=graph, nodetype=int, data=(('weight',float),))
     elif format is Formats.other:
         pass
 
-    #check if graph has weights
+    #check if graph has weights - if not add uniform
     if not is_weighted(graph):
         from networkx import set_edge_attributes
         set_edge_attributes(graph, values = 1, name = 'weight')

LINE1/line.py
@@ -5,11 +5,14 @@ Resources used:
     https://github.com/tangjianpku/LINE
 """
 from networkx import Graph, DiGraph, read_edgelist
+from networkx.algorithms.graph_hashing import weisfeiler_lehman_graph_hash
 from networkx.classes.function import is_weighted, set_edge_attributes
 import numpy as np
 from math import exp
 import threading
 from time import time
+from os.path import isfile
+import pickle 
 
 #constants (values taken from original implementation)
 N_THREADS = 4 #number of thread for asynchronous gradient descent
@@ -189,16 +192,16 @@ def line_thread(seed, graph):
         graph (Graph): the original graph
     """
     print(f"Thread {seed} has been started")
+    thread_id = seed
     global embedding
     count = 0
     last_print = 0 #every now and then print whats happening
     while count <= N_SAMPLES  / N_THREADS + 2:
-        print("Done cycle ", count)
         #give sign of life and update rho
         if count - last_print > 1e3:
             current_sample_count = count - last_print
             last_print = count
-            print(f"Thread {seed} had done {count} iterations and is willing to do more")
+            print(f"Thread {thread_id} had done {count} iterations and is willing to do more")
             rho = initial_rho * (1 - current_sample_count / (N_SAMPLES - 1))
             if rho < initial_rho * 1e-4:
                 rho = initial_rho * 1e-4
@@ -206,9 +209,8 @@ def line_thread(seed, graph):
         #sample an edge
         edge = sample_edge(graph, np.random.random(), np.random.random())
         u, v = list(graph.edges)[edge]
-        #todo: clean
-        lu = u# * EMBEDDING_DIMENSION
-        lv = v# * EMBEDDING_DIMENSION
+        lu = u
+        lv = v
 
         error_vector = [0 for _ in range(EMBEDDING_DIMENSION)]
         target, label = 0, 0
@@ -220,7 +222,7 @@ def line_thread(seed, graph):
                 seed, rand_number = Rand(seed)
                 target = negative_table[rand_number]
                 label = 0
-            lv = target# * EMBEDDING_DIMENSION
+            lv = target
             update(lu, lv, error_vector, label)
 
         for i, val in enumerate(error_vector):
@@ -264,7 +266,15 @@ def line1(graph: DiGraph):
     #check if graph has weights
     if not is_weighted(graph):
         set_edge_attributes(graph, values = 1, name = 'weight')
+
     embedding = {}
+    #check if embedding has already been done (embedding will be saved in the end)
+    graph_filename = "line1_" + weisfeiler_lehman_graph_hash(graph) + ".txt"
+    if isfile(graph_filename):
+        with open(graph_filename, "r") as file:
+            embedding = pickle.loads(file.read())
+        return embedding
+            
     generate_alias_table(graph)
     generate_negative_table(graph)
     generate_sigmoid_table()
@@ -277,12 +287,14 @@ def line1(graph: DiGraph):
         thread.join()
     
     print(f"Embedding process ended. Total time was {time() - t_0}")
+    with open(graph_filename, 'w') as file:
+        pickle.dump(embedding, file)
     return embedding
 
 #test this method
 if __name__ == "__main__":
-    graph = read_edgelist("./Datasets/WikiVote.txt", create_using=Graph(), nodetype=int, data=(('weight',float),))
+    from Datasets.datasets import Datasets, get_graph
 
-    embedding = line1(graph)
+    embedding = line1(get_graph(Datasets.WikiVote))
 
-    # print(embedding)
+    print(embedding)